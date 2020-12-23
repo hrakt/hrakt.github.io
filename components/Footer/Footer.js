99e1147aa4c4954651e9eb7121c09e0aa4082c42
Hrak Torousian <hraktorres@gmail.com>
2020-12-22 20:59:28 -0800

    Fix footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import styles from "../Footer/Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles["container"]}>
+      <div>
+        <p>© Hrak Torousian. All rights reserved. 2020</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
