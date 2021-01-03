8ddc6da148807f9a3a6b3ea03a8eedda3459f338
Hrak Torousian <hraktorres@gmail.com>
2021-01-02 18:26:51 -0800

    Update Work layout

--- components/Work/Work.js
+++ components/Work/Work.js
@@ -1,29 +1,6 @@
 import styles from "../Work/Work.module.scss";
 import classNames from "classnames/bind";
-
-const workArr = [
-  {
-    imgSrc: "/Tillamook.png",
-    type: "Website",
-    title: "Tillamook",
-    description:
-      "Worked on redesign project for Tillamook.com. Took care of many for future use by the team of developers. Learned the methods of using storybook components, commerace technologies and improving SEO and Accesibility",
-  },
-  {
-    imgSrc: "/Toyota.png",
-    type: "Web App",
-    title: "Toyota",
-    description:
-      "Participated in development of a web experience meant to showcase Toyota's latest model. The panorama experience included many overlay info displays which were created by me.",
-  },
-  {
-    imgSrc: "/GrandNash.png",
-    type: "Website",
-    title: "Grand + Nash",
-    description:
-      "Took part in one of my first major agency projects. Worked in an AGILE enviornment alongside designers. Helped in creation of a unique ground up experience for a real estate business",
-  },
-];
+import workArr from "./workArr";
 
 var align;
 
@@ -47,7 +24,7 @@ const Work = () => {
               <div>{item.description}</div>
             </div>
             <div className={styles["item__imageBox"]}>
-              <img src={item.imgSrc} width="500" height="250" />
+              <img src={item.imgSrc} width="400" height="200" />
             </div>
           </div>
         );
