aec0f26eb57fb0911c13c3c4ba006de8aa55d144
Hrak Torousian <hraktorres@gmail.com>
2021-01-04 17:18:36 -0800

    Fix about section spacing

--- components/About/About.js
+++ components/About/About.js
@@ -5,18 +5,22 @@ const About = () => {
     <div className={styles["container"]}>
       <div className={styles["image__container"]}>
         <div className={styles["image__box"]}>
-          <img src="Portrait.jpg" alt="Portrait" width="390" height="520" />
+          <img src="Portrait.jpg" alt="Portrait" width="390" />
         </div>
       </div>
+      <div className={styles["seperator"]} />
       <div className={styles["text__container"]}>
         <h1>About me</h1>
-        <p>
-          Experienced Web Developer with a demonstrated history of work in the
-          web agency and tech startup industry. Always curious and eager about
-          gaining more knowledge in modern-day web and software technologies and
-          beyond, to create eye-catching experiences and solve real-world
-          problems.
-        </p>
+        <div className={styles["text__paragraph"]}>
+          <p>
+            Experienced Web Developer with a demonstrated history of work in the
+            web agency and tech startup industry. Always curious and eager about
+            gaining more knowledge in modern-day web and software technologies
+            and beyond, to create eye-catching experiences and solve real-world
+            problems.
+          </p>
+        </div>
+
         <a href="Resume.docx" download>
           Download my resume
         </a>
