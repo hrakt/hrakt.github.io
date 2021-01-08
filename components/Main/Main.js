c7fd3825d13acd7b19dc12a8e14db14e7c227c0c
Hrak Torousian <hraktorres@gmail.com>
2021-01-08 15:00:19 -0800

    Create toggle

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,31 +1,38 @@
+import { useState } from "react";
 import styles from "../Main/Main.module.scss";
-// import Button from '../common/Button';
+import DarkModeToggle from "./DarkModeToggle/index";
 import { motion } from "framer-motion";
 
 const Main = () => {
+  const [toggleState, setStogelState] = useState(false);
+  console.log(toggleState);
+  const toggle = () => {
+    setStogelState(!toggleState);
+  };
   return (
-    <motion.div
-      animate={{ opacity: [0, 1] }}
-      transition={{ duration: 0.5 }}
-      className={styles["container"]}
-    >
-      <div className={styles["main-container"]}>
-        <h1 className={styles["main-container__heading1"]}>Hi, I'm Hrak </h1>
-        <h2 className={styles["main-container__heading2"]}>
-          <div className={styles["slidingVertical"]}>
-            <span>Based in LA</span>
-            <span>Self-Taught</span>
-            <span>Experienced</span>
-            <span>Creative</span>
-          </div>
-        </h2>
-        <h2 className={styles["main-container__heading2"]}>
-          Full Stack Developer
-        </h2>
-
-        {/* <Button type="external" href="/about" title="About Me" /> */}
-      </div>
-    </motion.div>
+    <>
+      <motion.div
+        animate={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className={styles["container"]}
+      >
+        <div className={styles["main-container"]}>
+          <h1 className={styles["main-container__heading1"]}>Hi, I'm Hrak </h1>
+          <h2 className={styles["main-container__heading2"]}>
+            <div className={styles["slidingVertical"]}>
+              <span>Based in LA</span>
+              <span>Self-Taught</span>
+              <span>Experienced</span>
+              <span>Creative</span>
+            </div>
+          </h2>
+          <h2 className={styles["main-container__heading2"]}>
+            Full Stack Developer
+          </h2>
+        </div>
+      </motion.div>
+      <DarkModeToggle toggle={toggle} />
+    </>
   );
 };
 export default Main;
