7afa9bf2109bd37d591f4a27558bf866945b2aee
Hrak Torousian <hraktorres@gmail.com>
2021-01-07 22:04:52 -0800

    Update landing

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -10,25 +10,17 @@ const Main = () => {
       className={styles["container"]}
     >
       <div className={styles["main-container"]}>
-        <h1 className={styles["main-container__heading1"]}>
-          Hi, I'm Hrak{" "}
-          <motion.div
-            className={styles["main-container__emoji"]}
-            animate={{ rotate: [0, 0, 90, 90, 0] }}
-            transition={{ duration: 2 }}
-          >
-            👋
-          </motion.div>
-        </h1>
+        <h1 className={styles["main-container__heading1"]}>Hi, I'm Hrak </h1>
         <h2 className={styles["main-container__heading2"]}>
-          I'm a
           <div className={styles["slidingVertical"]}>
-            <span>self-taught</span>
-            <span>curious</span>
-            <span>creative</span>
-            <span>energetic</span>
+            <span>Based in LA</span>
+            <span>Self-Taught</span>
+            <span>Experienced</span>
+            <span>Creative</span>
           </div>
-          web developer
+        </h2>
+        <h2 className={styles["main-container__heading2"]}>
+          Full Stack Developer
         </h2>
 
         {/* <Button type="external" href="/about" title="About Me" /> */}
