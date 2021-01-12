c884288bbec34397799ea2cd4359d754f8d662f1
Hrak Torousian <hraktorres@gmail.com>
2021-01-11 18:28:13 -0800

    Create work array file

---
+++ components/Work/workArr.js
@@ -0,0 +1,25 @@
+const workArr = [
+  {
+    imgSrc: "/Tillamook.png",
+    type: "Website",
+    title: "Tillamook",
+    description:
+      "Worked on redesign project for Tillamook.com. Took care of many for future use by the team of developers. Learned the methods of using storybook components, commerace technologies and improving SEO and Accesibility",
+  },
+  {
+    imgSrc: "/Toyota.png",
+    type: "Web App",
+    title: "Toyota",
+    description:
+      "Participated in development of a web experience meant to showcase Toyota's latest model. The panorama experience included many overlay info displays which were created by me.",
+  },
+  {
+    imgSrc: "/GrandNash.png",
+    type: "Website",
+    title: "Grand + Nash",
+    description:
+      "Took part in one of my first major agency projects. Worked in an AGILE enviornment alongside designers. Helped in creation of a unique ground up experience for a real estate business",
+  },
+];
+
+export default workArr;
