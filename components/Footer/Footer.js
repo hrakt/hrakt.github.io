49d93f4a522a253ce4b716d6b3f36110e784e628
Hrak Torousian <hraktorres@gmail.com>
2021-01-21 12:23:24 -0800

    Update classnames for Main and Footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,7 +2,7 @@ import styles from "../Footer/Footer.module.scss";
 
 const Footer = () => {
   return (
-    <div className={styles["container"]}>
+    <div className={styles.container}>
       <div>
         <p>© Hrak Torousian. All rights reserved. 2020</p>
       </div>
--- components/Main/Main.js
+++ components/Main/Main.js
@@ -2,22 +2,18 @@ import styles from "../Main/Main.module.scss";
 
 const Main = () => {
   return (
-    <div className={styles["container"]}>
-      <div className={styles["main-container"]}>
-        <h1 className={styles["main-container__heading1"]}>Hi, I'm Hrak </h1>
-        <h2 className={styles["main-container__heading2"]}>
-          <div className={styles["slidingVertical"]}>
+    <div className={styles.container}>
+      <div className={styles.mainContainer}>
+        <h1 className={styles.mainContainer__heading1}>Hi, I'm Hrak </h1>
+        <h2 className={styles.mainContainer__heading2}>
+          <div className={styles.slidingVertical}>
             <span>Based in LA</span>
             <span>Self-Taught</span>
             <span>Experienced</span>
             <span>Creative</span>
           </div>
         </h2>
-        <h2 className={styles["main-container__heading2"]}>
-          Full Stack Developer
-        </h2>
-
-        {/* <Button type="external" href="/about" title="About Me" /> */}
+        <h2 className={styles.mainContainer__heading2}>Full Stack Developer</h2>
       </div>
     </div>
   );
