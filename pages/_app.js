282f809541f8f9c387c73aa8721a979e0a3a1bf1
Hrak Torousian <hraktorres@gmail.com>
2021-01-25 08:41:40 -0800

    Update app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
+import withRedux from "next-redux-wrapper";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Provider store={}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+class MyApp {
+  static async getInitialProps({ Component, ctx }) {
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+
+    //Anything returned here can be accessed by the client
+    return { pageProps: pageProps };
+  }
+  render() {
+    const { Component, pageProps, store } = this.props;
+    return (
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    );
+  }
 }
 
-export default MyApp;
+const makeStore = () => store;
+
+export default withRedux(makeStore)(MyApp);
