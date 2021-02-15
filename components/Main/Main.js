57cdee56350e2e68af0610459f5f7e261e294760
Hrak Torousian <hraktorres@gmail.com>
2021-02-15 08:26:15 -0800

    Remove unused html

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -8,14 +8,6 @@ const Main = () => {
       <div className={styles.container}>
         <div className={styles.mainContainer}>
           <h1 className={styles.mainContainer__heading1}>Hrak Torusian </h1>
-          {/* <h2 className={styles.mainContainer__heading2}>
-            <div className={styles.slidingVertical}>
-              <span>Based in LA</span>
-              <span>Self-Taught</span>
-              <span>Experienced</span>
-              <span>Creative</span>
-            </div>
-          </h2> */}
           <h2 className={styles.mainContainer__heading2}>
             Full Stack Developer
           </h2>
