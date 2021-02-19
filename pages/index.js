cb949a353c89db4a81652d62e4c416dbcb90ca29
Hrak Torousian <hraktorres@gmail.com>
2021-02-19 09:14:49 -0800

    Add logo icon to html

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,7 @@ export default function Home() {
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="Logo.png" />
           <script
             src="https://cdnjs.cloudflare.com/ajax/libs/three.js/109/three.min.js"
             integrity="sha256-Wua0LsQkGDEeIkbmBNjj9wcThJh5uCD6R8+5YQhXveQ="
