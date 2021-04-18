1d6740ebcef9328e67ae33482ba34caa058da98e
Hrak Torousian <hraktorres@gmail.com>
2021-04-17 22:42:23 -0700

    Remove blog post page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,42 +1,41 @@
 import DefaultLayout from "../../layouts/index";
 import BlogPost from "../../components/Blog/BlogPost";
 
-let client = require("contentful").createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-});
+// let client = require("contentful").createClient({
+//   space: process.env.CONTENTFUL_SPACE_ID,
+//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+// });
 
-export async function getStaticPaths() {
-  let data = await client.getEntries({
-    content_type: "post",
-  });
+// export async function getStaticPaths() {
+//   let data = await client.getEntries({
+//     content_type: "post",
+//   });
 
-  return {
-    paths: data.items.map((item) => ({
-      params: { slug: item.fields.slug },
-    })),
-    fallback: true,
-  };
-}
-export async function getStaticProps({ params }) {
-  let data = await client.getEntries({
-    content_type: "post",
-    "fields.slug": params.slug,
-  });
+//   return {
+//     paths: data.items.map((item) => ({
+//       params: { slug: item.fields.slug },
+//     })),
+//     fallback: true,
+//   };
+// }
 
-  return {
-    props: {
-      post: data.items[0],
-    },
-  };
-}
+// export async function getStaticProps({ params }) {
+//   let data = await client.getEntries({
+//     content_type: "post",
+//     "fields.slug": params.slug,
+//   });
 
-export default function Post({ post }) {
+//   return {
+//     props: {
+//       post: data.items[0],
+//     },
+//   };
+// }
+
+export default function Post({}) {
   return (
     <div>
-      <DefaultLayout>
-        <BlogPost post={post} />
-      </DefaultLayout>
+      <DefaultLayout>{/* <BlogPost post={post} /> */}</DefaultLayout>
     </div>
   );
 }
